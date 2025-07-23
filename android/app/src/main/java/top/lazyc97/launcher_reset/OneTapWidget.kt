fc2a394e3def0afd0acce3ef50391e2ca0c372fc
nghduc97 <lazyc97@protonmail.com>
2025-07-23 23:29:30 +0700

    remove redundant semicolon

--- android/app/src/main/java/top/lazyc97/launcher_reset/OneTapWidget.kt
+++ android/app/src/main/java/top/lazyc97/launcher_reset/OneTapWidget.kt
@@ -10,7 +10,7 @@ import android.widget.Toast
 import io.flutter.Log
 import java.util.concurrent.TimeUnit
 
-const val WIDGET_KILL_LAUNCHER_INTENT = "WIDGET_KILL_LAUNCHER_INTENT";
+const val WIDGET_KILL_LAUNCHER_INTENT = "WIDGET_KILL_LAUNCHER_INTENT"
 
 /**
  * Implementation of App Widget functionality.
@@ -38,17 +38,17 @@ class OneTapWidget : AppWidgetProvider() {
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == WIDGET_KILL_LAUNCHER_INTENT) {
             try {
-                val psProc = ProcessBuilder("su", "-c", "ps -o pid,cmd").start();
-                psProc.waitFor(10, TimeUnit.SECONDS);
+                val psProc = ProcessBuilder("su", "-c", "ps -o pid,cmd").start()
+                psProc.waitFor(10, TimeUnit.SECONDS)
                 val psOut = psProc.inputStream.bufferedReader().use { it.readText() }
                 val lines = psOut.split("\n")
 
                 val spaceRegex = Regex("\\s+")
-                var foundPid = "";
+                var foundPid = ""
                 for (line in lines) {
                     if (line.endsWith("oid.launcher3")) {
                         val parts = line.split(spaceRegex, limit = 2)
-                        foundPid = parts[0];
+                        foundPid = parts[0]
                         Log.i("PidLookup", "name=${parts[1]} pid=${foundPid}")
                         break
                     }
@@ -80,8 +80,8 @@ internal fun updateAppWidget(
 
     val intent = Intent(context, OneTapWidget::class.java)
     intent.action = WIDGET_KILL_LAUNCHER_INTENT
-    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
-    views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+    val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+    views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
